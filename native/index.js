dba6a4b7cb06a4653b9e2869c572dbaa17455abe
guofangchao <guofangchao@daling.com>
2021-05-28 17:28:31 +0800

    修改默认文件

--- native/index.js
+++ native/index.js
@@ -4,33 +4,20 @@ const {
     Tray,
     screen,
     ipcMain,
-} = require("electron");
+} = require('electron');
 
-const path = require("path");
-const isDev = require("electron-is-dev");
-// const isDev = true;
+const path = require('path');
 
-const rootFolder = path.join(__dirname, "..");
+const rootFolder = path.join(__dirname, '..');
+const isDev = process.env.NODE_ENV === 'development';
 
 // 保持一个对于 window 对象的全局引用，不然，当 JavaScript 被 GC，
 // window, tray 会被自动地关闭
 let mainWindow = null;
 let tray = null;
 
-const windowWidth = isDev ? 1400 : 400;
+const windowWidth = 1000;
 const windowHeight = 600;
-
-function setWindowPosition() {
-    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
-
-    let windowX, windowY;
-
-    windowX = (width - windowWidth) / 2;
-    windowY = (height - windowHeight) / 2;
-
-    // Set window position
-    //   mainWindow.setPosition(windowX, windowY);
-}
 
 function createWindow() {
     // 创建浏览器窗口。
@@ -38,31 +25,25 @@ function createWindow() {
         width: windowWidth,
         height: windowHeight,
         show: false,
-        frame: false,
-        resizable: isDev,
-        backgroundColor: "#403F4D",
-        icon: path.join(rootFolder, "assets", "icon.png"),
+        backgroundColor: '#403F4D',
+        icon: path.join(rootFolder, 'assets', 'icon.png'),
         webPreferences: {
             nodeIntegration: false,
             worldSafeExecuteJavaScript: false,
-            preload: path.join(__dirname, "preload.js"),
+            preload: path.join(__dirname, 'preload.js'),
             devTools: true,
         },
     });
 
-    const startURL = isDev ? "http://localhost:3000" : `file://${path.join(rootFolder, "/build/index.html")}`;
+    const startURL = isDev ? 'http://localhost:3000' : `file://${path.join(rootFolder, '/build/index.html')}`;
     mainWindow.loadURL(startURL);
-    // 加载应用的 index.html
-    // mainWindow.loadFile(path.join(rootFolder, 'public/index.html'))
 
-    setWindowPosition();
+    mainWindow.once('ready-to-show', () => mainWindow.show());
 
-    mainWindow.once("ready-to-show", () => mainWindow.show());
+    tray = new Tray(path.join(rootFolder, 'assets', 'icon.png'));
+    tray.setToolTip('Click to access OneCopy');
 
-    tray = new Tray(path.join(rootFolder, "assets", "icon.png"));
-    tray.setToolTip("Click to access OneCopy");
-
-    tray.on("click", () => {
+    tray.on('click', () => {
         mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
     });
 }
@@ -72,7 +53,7 @@ const isSingleInstance = app.requestSingleInstanceLock();
 if (!isSingleInstance) {
     app.quit();
 } else {
-    app.on("second-instance", () => {
+    app.on('second-instance', () => {
         // Someone tried to run a second instance, we should focus our window.
         if (mainWindow) {
             if (mainWindow.isMinimized()) mainWindow.restore();
@@ -81,26 +62,26 @@ if (!isSingleInstance) {
     });
 
     // 当所有窗口被关闭了，退出。
-    app.on("window-all-closed", function () {
+    app.on('window-all-closed', function () {
         // 在 OS X 上，通常用户在明确地按下 Cmd + Q 之前
         // 应用会保持活动状态
-        if (process.platform !== "darwin") app.quit();
+        if (process.platform !== 'darwin') app.quit();
     });
 
     // 当 Electron 完成了初始化并且准备创建浏览器窗口的时候
     // 这个方法就被调用
-    app.on("ready", () => {
+    app.on('ready', () => {
         // 调用创建浏览器窗口。
         createWindow();
 
         // Set App ID for notifications
-        app.setAppUserModelId("com.hiroshifuu.onecopy-electron");
+        app.setAppUserModelId('com.hiroshifuu.onecopy-electron');
 
         // 打开开发工具
         if (isDev) mainWindow.openDevTools();
 
         // 当 window 被关闭，这个事件会被发出
-        mainWindow.on("closed", function () {
+        mainWindow.on('closed', function () {
             // 取消引用 window 对象，如果你的应用支持多窗口的话，
             // 通常会把多个 window 对象存放在一个数组里面，
             // 但这次不是。
@@ -108,7 +89,7 @@ if (!isSingleInstance) {
         });
     });
 
-    app.on("activate", function () {
+    app.on('activate', function () {
         // On macOS it's common to re-create a window in the app when the
         // dock icon is clicked and there are no other windows open.
         if (mainWindow === null || BrowserWindow.getAllWindows().length === 0) {
@@ -117,7 +98,7 @@ if (!isSingleInstance) {
     });
 
     // Listen for 'open-main-window' event from renderer process
-    ipcMain.on("open-main-window", () => {
+    ipcMain.on('open-main-window', () => {
         // Set always on top to true so that
         // mainwindow can be viewed on top of other apps
         mainWindow.setAlwaysOnTop(true);
@@ -127,12 +108,12 @@ if (!isSingleInstance) {
         mainWindow.setAlwaysOnTop(false);
     });
 
-    ipcMain.on("minimize-app", () => {
+    ipcMain.on('minimize-app', () => {
         // Hide the main window
         mainWindow.hide();
     });
 
-    ipcMain.on("quit-app", () => {
+    ipcMain.on('quit-app', () => {
         // Quit the app and close all windows
         app.quit();
     });
